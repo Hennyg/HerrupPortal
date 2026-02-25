b5c58f188aa88a191833fbd06e2d1ec0a7f21bb2
Hennyg <henrik.nygaard@live.com>
2026-02-25 11:03:59 +0100

    Update index.js

--- api/links-admin/index.js
+++ api/links-admin/index.js
@@ -3,13 +3,15 @@ const { dvFetch } = require("../_dv");
 function json(context, status, body) {
   context.res = { status, headers: { "Content-Type": "application/json; charset=utf-8" }, body };
 }
+function esc(s){ return String(s ?? "").replace(/'/g,"''"); }
 
 module.exports = async function (context, req) {
   try {
-    const q = `EntityDefinitions?$select=LogicalName,EntitySetName,DisplayName&$filter=contains(LogicalName,'portallink')`;
+    const logical = "cr175_lch_portallink";
+    const q = `EntityDefinitions?$select=LogicalName,EntitySetName&$filter=LogicalName eq '${esc(logical)}'`;
     const data = await dvFetch(q);
     return json(context, 200, data);
   } catch (e) {
-    return json(context, e.status || 500, { ok:false, message: e.message, status: e.status, data: e.data, stack: e.stack });
+    return json(context, e.status || 500, { message: e.message, status: e.status, data: e.data, stack: e.stack });
   }
 };
