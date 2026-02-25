38529d4eabd3c14d20e38e6a2dcd5ae020a26608
Hennyg <henrik.nygaard@live.com>
2026-02-25 11:11:23 +0100

    Update index.js

--- api/links-admin/index.js
+++ api/links-admin/index.js
@@ -1,17 +1,24 @@
 const { dvFetch } = require("../_dv");
 
-function json(context, status, body) {
-  context.res = { status, headers: { "Content-Type": "application/json; charset=utf-8" }, body };
-}
-function esc(s){ return String(s ?? "").replace(/'/g,"''"); }
-
 module.exports = async function (context, req) {
   try {
     const logical = "cr175_lch_portallink";
-    const q = `EntityDefinitions?$select=LogicalName,EntitySetName&$filter=LogicalName eq '${esc(logical)}'`;
-    const data = await dvFetch(q);
-    return json(context, 200, data);
+
+    // Key lookup i stedet for $filter
+    const data = await dvFetch(
+      `EntityDefinitions(LogicalName='${logical}')?$select=LogicalName,EntitySetName,SchemaName`
+    );
+
+    context.res = {
+      status: 200,
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      body: { ok: true, data }
+    };
   } catch (e) {
-    return json(context, e.status || 500, { message: e.message, status: e.status, data: e.data, stack: e.stack });
+    context.res = {
+      status: e.status || 500,
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      body: { ok: false, message: e.message, status: e.status, data: e.data, stack: e.stack }
+    };
   }
 };
