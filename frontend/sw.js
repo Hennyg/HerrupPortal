6d870fb0d49e201c2a9d09c1f9eaf879016ae66d
Hennyg <henrik.nygaard@live.com>
2026-02-26 11:09:28 +0100

    Update sw.js

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,5 +1,3 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-
-// Nogle Chrome-versioner bliver mere “sikre” på installbarhed når fetch findes
 self.addEventListener("fetch", () => {});
