cfc20c42fb1d756f66952fa4ac3c000f03e82615
Hennyg <henrik.nygaard@live.com>
2026-02-26 10:08:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,2 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
-
-<script>
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js');
-}
-</script>
