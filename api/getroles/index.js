d957301ee149880c82ac98fb342edf7a6ec97039
Hennyg <henrik.nygaard@live.com>
2026-03-02 11:16:15 +0100

    Create index.js

---
+++ api/getroles/index.js
@@ -0,0 +1,7 @@
+module.exports = async function (context, req) {
+  context.res = {
+    status: 200,
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+    body: { ok: true, fn: "getroles" }
+  };
+};
